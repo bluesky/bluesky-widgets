5533b68769d05ec97a01db0d834ffd7163e33101
Dan Allan <dallan@bnl.gov>
2020-07-14 12:10:18 -0400

    Add RunList stub.

--- stream_widgets/examples/viewer_model.py
+++ stream_widgets/examples/viewer_model.py
@@ -1,12 +1,14 @@
 from stream_widgets.components.search.searches import SearchList
+from stream_widgets.components.runs import RunList
 from stream_widgets.examples.utils.add_search_mixin import AddSearchMixin
 
 
 class ViewerModel(AddSearchMixin):
     """
-    Compose various models (search input, search results, ...) into one object.
+    Compose various models into one object to provide a nice user API.
     """
     def __init__(self, title):
         self.title = title
         self.searches = SearchList()
+        self.runs = RunList()
         super().__init__()
