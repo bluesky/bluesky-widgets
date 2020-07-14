38ab13365ed7dae437b8b77393707d2170d5eb7c
Dan Allan <dallan@bnl.gov>
2020-07-14 14:21:40 -0400

    Test for widget leaks.

---
+++ stream_widgets/tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from qtpy.QtWidgets import QApplication
+
+
+@pytest.fixture
+def qtbot(qtbot):
+    """A modified qtbot fixture that makes sure no widgets have been leaked."""
+    # Adapted from napari
+    initial = QApplication.topLevelWidgets()
+    yield qtbot
+    QApplication.processEvents()
+    leaks = set(QApplication.topLevelWidgets()).difference(initial)
+    if leaks:
+        raise AssertionError(f'Widgets leaked!: {leaks}')
+
+
