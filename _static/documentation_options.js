affc1ae9f3dcec4832f9f06c0389dd351d6819fc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-12 02:28:56 +0000

    Deploy bluesky/bluesky-widgets to github.com/bluesky/bluesky-widgets.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.4.post32+gaa252d8',
+    VERSION: '0.0.4.post43+g001b59c',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
