dcd58623c2a64ce0e40a29a355c2f646399481c1
gbischof <bisc8233@gmail.com>
2020-11-12 14:53:12 -0500

    set state on the parent

--- bluesky_widgets/examples/napari_dock_widgets.py
+++ bluesky_widgets/examples/napari_dock_widgets.py
@@ -3,26 +3,47 @@ import napari
 from bluesky_widgets.examples.qt_search import SearchListWithButton, QtSearchListWithButton
 from bluesky_widgets.examples.utils.add_search_mixin import extract_results_row_from_run
 from bluesky_widgets.examples.utils.generate_msgpack_data import get_catalog
-from bluesky_widgets.models.search import Search
+from bluesky_widgets.models.search import Search, SearchList
 
-headings = (
-    "Unique ID",
-    "Transient Scan ID",
-    "Plan Name",
-    "Start Time",
-    "Duration",
-    "Exit Status",
-)
+
+class SearchListWithButton(SearchList):
+    """
+    A SearchList model with a method to handle a click event.
+    """
+    def __init__(self, *args, process=lambda: print('aaaaa'), **kwargs):
+        super().__init__(*args, **kwargs)
+        self.process = process
+
+    def process(self):
+        self.process()
+
+        #for uid, run in self.active.selection_as_catalog.items():
+            # Pretend to kick off data processing or something.
+        #    print(
+        #        f"Processing Run {uid[:8]} (scan_id={run.metadata['start']['scan_id']})"
+        #    )
+
 
 class NapariDatabroker(napari.Viewer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.searches = SearchListWithButton()
+        self.searches = SearchListWithButton(process=self.process)
+        self.state = None
+
 
     def add_search(self, catalog, columns=extract_results_row_from_run):
         """
         Add a new Search form.
         """
+        headings = (
+                    "Unique ID",
+                    "Transient Scan ID",
+                    "Plan Name",
+                    "Start Time",
+                    "Duration",
+                    "Exit Status",
+                   )
+
         search = Search(catalog, columns=(headings, extract_results_row_from_run))
         self.searches.append(search)
 
@@ -32,6 +53,10 @@ class NapariDatabroker(napari.Viewer):
         Convenience for accessing the currently-active Search form.
         """
         return self.searches.active
+
+    def process(self):
+        self.state = 123123
+        print(self.state)
 
 
 with napari.gui_qt():
