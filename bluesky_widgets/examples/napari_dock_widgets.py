8b8d1d34a29d4b501341ec89609bb301cab416ee
gbischof <bisc8233@gmail.com>
2020-11-13 12:02:25 -0500

    add generate_thumbnail

--- bluesky_widgets/examples/napari_dock_widgets.py
+++ bluesky_widgets/examples/napari_dock_widgets.py
@@ -1,35 +1,71 @@
 import napari
 import numpy as np
 
-from bluesky_widgets.examples.qt_search import SearchListWithButton, QtSearchListWithButton
+#from bluesky_widgets.examples.qt_search import QtSearchListWithButton
 from bluesky_widgets.examples.utils.add_search_mixin import extract_results_row_from_run
 from bluesky_widgets.examples.utils.generate_msgpack_data import get_catalog
 from bluesky_widgets.models.search import Search, SearchList
+from bluesky_widgets.utils.event import Event
+from qtpy.QtWidgets import QWidget, QPushButton, QVBoxLayout
+from bluesky_widgets.qt.search import QtSearches
 
 
 class SearchListWithButton(SearchList):
     """
     A SearchList model with a method to handle a click event.
+
+    If we were to add a method of this class called on_view_images,
+    then it would automatically receive view_images events without
+    needing to use view_images.connect.
     """
-    def __init__(self, *args, process=lambda: print('aaaaa'), **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.process = process
 
-    def process(self):
-        self.process()
+        # Register an event emitter for process events.
+        # The kwarg view_images defines the name of the events that are emitted.
+        # Event is the event type.
+        self.events.add(view_images=Event)
+    
 
-        #for uid, run in self.active.selection_as_catalog.items():
-            # Pretend to kick off data processing or something.
-        #    print(
-        #        f"Processing Run {uid[:8]} (scan_id={run.metadata['start']['scan_id']})"
-        #    )
+class QtSearchListWithButton(QWidget):
+    """
+    A view for SearchListWithButton.
+
+    Combines the QtSearches widget with a button that processes the selected Runs.
+    """
+
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(QtSearches(model))
+
+        # Add a button that does something with the currently-selected Runs
+        # when you click it.
+        go_button = QPushButton("Process Selected Runs")
+        layout.addWidget(go_button)
+
+        # Register a callback (slot) for the button qt click signal.
+        go_button.clicked.connect(self.on_click)
+
+    def on_click(self):
+        """
+        Receive the qt signal, and emmit a view_images event.
+        
+        Add whatever we want to the event by passing it as a kwarg, in this case the images kwarg.
+        Can access images in the callback by accessing Event.images.
+        """
+        self.model.events.view_images(images="asdasdasda")
 
 
 class NapariDatabroker(napari.Viewer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.searches = SearchListWithButton(process=self.process)
-        self.state = None
+        self.searches = SearchListWithButton()
+
+        # Register the process event callback.
+        self.searches.events.view_images.connect(self.load_images_from_run)
 
     def add_search(self, catalog, columns=extract_results_row_from_run):
         """
@@ -54,9 +90,13 @@ class NapariDatabroker(napari.Viewer):
         """
         return self.searches.active
 
-    def process(self):
-        image = np.random.rand(200, 200)
-        self.add_image(image)
+    def load_images_from_run(self, view_images):
+        """
+        Handle a view_images event.
+        """
+        event.thing
+        self.image = np.random.rand(200, 200)
+        self.add_image(self.image)
 
 
 with napari.gui_qt():
---
+++ bluesky_widgets/examples/utils/get_run_images.py
@@ -0,0 +1,44 @@
+import sys
+from imageio import imsave
+
+
+def generate_thumbnail(run):
+    """
+    Return one thumbnail image to represent a Run if any image-like data can be found.
+    
+    Logs details to the stderr.
+    
+    Parameters
+    ----------
+    run: BlueskyRun
+    
+    Returns
+    -------
+    image: np.array
+    """
+
+    uid = run.metadata['start']['uid']
+    if 'primary' in run:
+        stream_name = 'primary'
+    elif list(run):
+        # Just grab the first stream.
+        stream_name = list(run)[0]
+    else:
+        print(f"No image data found in Run {uid:.8}", file=sys.stderr)
+    dataset = run[stream_name].to_dask()
+    # Find the first column that looks like an image.
+    # Grab a slice from the middle because that is most likely to be interesting.
+    for column in dataset:
+        xarr = dataset[column]
+        if xarr.ndim == 3:  # a column of single images
+            image = dataset[column][xarr.shape[0] // 2]
+            break
+        elif xarr.ndim == 4:  # a column of stacks of images (like Area Detector gives)
+            image = dataset[column][xarr.shape[0] // 2, xarr.shape[1] // 2]
+            break
+    else:
+        print(f"No image data found in Run {uid:.8}", file=sys.stderr)
+        return None
+    imsave(path, image)
+    print(f"Wrote {column!r} from Run {uid:.8} to {path}", file=sys.stderr)
+    return path
