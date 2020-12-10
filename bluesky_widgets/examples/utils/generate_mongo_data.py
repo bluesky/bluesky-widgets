6b8e996befe415feb5917d2e79235e611d884123
Dan Allan <dallan@bnl.gov>
2020-12-10 09:33:55 -0500

    Place mongodb data generator next to msgpack one.

---
+++ bluesky_widgets/examples/utils/generate_mongo_data.py
@@ -0,0 +1,29 @@
+# Based on databroker/examples/generate_mongo_data.py
+import logging
+from suitcase.mongo_normalized import Serializer
+from bluesky import RunEngine
+from bluesky.plans import count
+from ophyd.sim import det
+import uuid
+
+from databroker._drivers.mongo_normalized import BlueskyMongoCatalog
+
+
+def get_catalog():
+
+    RE = RunEngine()
+
+    mds = f"mongodb://localhost:27017/databroker-test-{uuid.uuid4()}"
+    fs = f"mongodb://localhost:27017/databroker-test-{uuid.uuid4()}"
+    serializer = Serializer(mds, fs)
+    RE(count([det]), serializer)
+    RE(count([det], 3), serializer)
+
+    logger = logging.getLogger("databroker")
+    logger.setLevel("DEBUG")
+    handler = logging.StreamHandler()
+    handler.setLevel("DEBUG")
+    logger.addHandler(handler)
+
+    catalog = BlueskyMongoCatalog(mds, fs)
+    return catalog
