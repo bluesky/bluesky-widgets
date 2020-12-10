5ae4371d722a44455b9be8612d3c7b2b0ca887f9
Dan Allan <dallan@bnl.gov>
2020-12-10 09:53:58 -0500

    Generate same data in mongo as in msgpack. Quiet.

--- bluesky_widgets/examples/utils/generate_mongo_data.py
+++ bluesky_widgets/examples/utils/generate_mongo_data.py
@@ -1,12 +1,15 @@
 # Based on databroker/examples/generate_mongo_data.py
-import logging
 from suitcase.mongo_normalized import Serializer
 from bluesky import RunEngine
-from bluesky.plans import count
-from ophyd.sim import det
+from bluesky.plans import count, scan
+from ophyd.sim import det, motor, SynSignal
+import numpy as np
 import uuid
 
 from databroker._drivers.mongo_normalized import BlueskyMongoCatalog
+
+
+random_img = SynSignal(func=lambda: np.random.random((5, 10, 10)), name="random_img")
 
 
 def get_catalog():
@@ -16,14 +19,9 @@ def get_catalog():
     mds = f"mongodb://localhost:27017/databroker-test-{uuid.uuid4()}"
     fs = f"mongodb://localhost:27017/databroker-test-{uuid.uuid4()}"
     serializer = Serializer(mds, fs)
-    RE(count([det]), serializer)
-    RE(count([det], 3), serializer)
-
-    logger = logging.getLogger("databroker")
-    logger.setLevel("DEBUG")
-    handler = logging.StreamHandler()
-    handler.setLevel("DEBUG")
-    logger.addHandler(handler)
+    for i in range(1, 5):
+        RE(scan([det], motor, -1, 1, 5 * i), serializer)
+    RE(count([random_img], 3), serializer)
 
     catalog = BlueskyMongoCatalog(mds, fs)
     return catalog
