3dca65be634cbc27284775fa8825d3d4f52733b6
Dan Allan <dallan@bnl.gov>
2021-01-06 17:35:44 -0500

    Redesign Auto into one class instead of many.

--- bluesky_widgets/models/auto_plot_builders.py
+++ bluesky_widgets/models/auto_plot_builders.py
@@ -1,88 +1,116 @@
-from abc import abstractproperty
 from collections import defaultdict
+from functools import partial
 
 from ..utils.dict_view import DictView
 from .utils import run_is_live_and_not_completed
-from .plot_builders import Images, Lines
 from .plot_specs import FigureSpecList
-from ._heuristics import infer_lines_to_plot, infer_images
+from .plot_builders import Lines, Images
+from .heuristics import infer_images, infer_lines_to_plot
 
 
-class Auto:
+class AutoPlotter:
     """
-    Base class for auto-plotters
+    Generate best-effort figures based on a heuristic.
 
-    EXPERIMENTAL! This is very like to change in a backward-incompatible way.
-    """
-
-    # Subclasses MUST override these properties.
-
-    @abstractproperty
-    def _plot_builder(self):
-        """
-        Plot-builder model, such as Lines.
-
-        Expected to implement:
-
+    Parameters
+    ----------
+    plot_builder : object
+        Plot-builder model, such as Lines or Images. Expected to implement:
         * add_run(run, **kwargs)  # kwargs are optional, builder-dependent
         * discard_run(run)
-        * figure
-        """
-        pass
+        * figure -> FigureSpec
 
-    @abstractproperty
-    def _heuristic(self):
-        """
+    heuristic : callable
         Callable that suggests what to plot
 
         Expected signature::
 
             f(run: BlueskyRun, steam_name: String) -> List[Dict]
 
-        where Dict is kwargs accepted by _plot_builder.__init__.
-        """
-        pass
+        where Dict is kwargs accepted by ``plot_builder.__init__``.
+    max_runs : Integer, optional
+        Default, ``None``, defers to the default of the ``plot_builder``.
+    extra_kwargs : Dict, optional
+        Passed to ``plot_buidler.__init__``.
 
-    # Subclasses MAY override this method.
+    Examples
+    --------
 
-    def _extra_kwargs(self):
-        "Passed to _plot_buidler.__init__"
-        return {}
+    Automaticaly plot some useful lines.
 
-    def __init__(self):
+    >>> from bluesky_widgets.models.plot_builders import Lines
+    >>> from bluesky_widgets.models.heuristics import infer_lines_to_plot
+    >>> model = AutoPlotter(Lines, infer_lines_to_plot)
+
+    Automatically plot all the images.
+
+    >>> from bluesky_widgets.models.plot_builders import Images
+    >>> from bluesky_widgets.models.heuristics import infer_images
+    >>> model = AutoPlotter(Images, infer_images)
+
+    See Also
+    --------
+    AutoLines
+    AutoImages
+    """
+
+    def __init__(self, plot_builder, heuristic, **extra_kwargs):
+        self._plot_builder = plot_builder
+        self._heuristic = heuristic
+        self._extra_kwargs = extra_kwargs or {}
+
         self.figures = FigureSpecList()
 
-        # Map key like ((x, y), stream_name) to plot_builder instance so configured.
-        self._key_to_instance = {}
+        # Map key like to plot_builder plot_builder so configured.
+        self._key_to_plot_builder = {}
         # Map FigureSpec UUID to key like ((x, y), stream_name)
         self._figure_to_key = {}
-        # Track inactive instances/figures which are no longer being updated
+        # Track inactive plot_builders/figures which are no longer being updated
         # with new Runs. Structure is a dict-of-dicts like:
-        # {key: {figure_uuid: instance, ...}, ...}
-        self._inactive_instances = defaultdict(dict)
+        # {key: {figure_uuid: plot_builder, ...}, ...}
+        self._inactive_plot_builders = defaultdict(dict)
         self.figures.events.removed.connect(self._on_figure_removed)
 
     @property
-    def keys_to_figures(self):
-        "Read-only mapping of each key to the active instance."
-        return DictView({v: k for k, v in self._figure_to_key.items()})
+    def plot_builder(self):
+        return self._plot_builder
 
-    def new_instance_for_key(self, key):
+    @property
+    def heuristic(self):
+        return self._heuristic
+
+    @property
+    def extra_kwargs(self):
+        return DictView(self._extra_kwargs)
+
+    @property
+    def plot_builders(self):
+        "Tuple of plot_builders corresponding to figures."
+        plot_builders = []
+        for figure in self.figures:
+            key = self._figure_to_key[figure.uuid]
+            plot_builder = self._key_to_plot_builder[key]
+            plot_builders.append(plot_builder)
+        return tuple(plot_builders)
+
+    def new_plot_builder_for_key(self, key):
         """
-        Make a new instance for a key.
+        Make a new plot_builder for a key.
 
-        If there is an existing one the instance and figure will remain but
+        If there is an existing one the plot_builder and figure will remain but
         will no longer be updated with new Runs. Those will go to a new
-        instance and figure, created here.
+        plot_builder and figure, created here.
         """
-        old_instance = self._key_to_instance.pop(key, None)
-        if old_instance is not None:
-            self._inactive_instances[key][old_instance.figure.uuid] = old_instance
-        instance = self._plot_builder(**dict(key, **self._extra_kwargs()))
-        self._key_to_instance[key] = instance
-        self._figure_to_key[instance.figure.uuid] = key
-        self.figures.append(instance.figure)
-        return instance
+        old_plot_builder = self._key_to_plot_builder.pop(key, None)
+        if old_plot_builder is not None:
+            self._inactive_plot_builders[key][
+                old_plot_builder.figure.uuid
+            ] = old_plot_builder
+        plot_builder = self._plot_builder(**dict(key, **self._extra_kwargs))
+        self._key_to_plot_builder[key] = plot_builder
+        self._figure_to_key[plot_builder.figure.uuid] = key
+        self.figures.append(plot_builder.figure)
+        return plot_builder
 
     def add_run(self, run, **kwargs):
         """
@@ -92,7 +120,7 @@ class Auto:
         ----------
         run : BlueskyRun
         **kwargs
-            Passed through to instance
+            Passed through to plot_builder
         """
         for stream_name in run:
             self._handle_stream(run, stream_name, **kwargs)
@@ -114,110 +142,39 @@ class Auto:
         ----------
         run : BlueskyRun
         """
-        for instance in self._key_to_instance.values():
-            instance.discard_run(run)
+        for plot_builder in self._key_to_plot_builder.values():
+            plot_builder.discard_run(run)
 
     def _handle_stream(self, run, stream_name, **kwargs):
-        "This examines a stream and adds this run to instances."
+        "This examines a stream and adds this run to plot_builders."
         for suggestion in self._heuristic(run, stream_name):
             # Make a hashable `key` out of the dict `suggestions`.
             key = tuple(suggestion.items())
             try:
-                instance = self._key_to_instance[key]
+                plot_builder = self._key_to_plot_builder[key]
             except KeyError:
-                instance = self.new_instance_for_key(key)
-            instance.add_run(run, **kwargs)
+                plot_builder = self.new_plot_builder_for_key(key)
+            plot_builder.add_run(run, **kwargs)
 
     def _on_figure_removed(self, event):
         """
         A figure was removed from self.figures.
 
-        Remove the relevant instance.
+        Remove the relevant plot_builder.
         """
         figure = event.item
         try:
             key = self._figure_to_key.pop(figure.uuid)
         except KeyError:
-            # This figure belongs to an inactive instance.
-            del self._inactive_instances[key][figure.uuid]
+            # This figure belongs to an inactive plot_builder.
+            del self._inactive_plot_builders[key][figure.uuid]
 
         else:
-            self._key_to_instance.pop(key)
+            self._key_to_plot_builder.pop(key)
 
 
-class AutoLines(Auto):
-    """
-    Automatically guess useful lines to plot. Show the last N runs (per figure).
+# For ease of use, preconfigure some AutoPlotters for various plot types with
+# general-purpose heuristics.
 
-    Parameters
-    ----------
-    max_runs : int
-        Number of Runs to plot at once, per figure
-
-    Attributes
-    ----------
-    figures : FigureSpecList[FigureSpec]
-    max_runs : int
-        Number of Runs to plot at once. This may be changed at any point.
-        (Note: Increasing it will not restore any Runs that have already been
-        removed, but it will allow more new Runs to be added.)
-    keys_to_figures : dict
-        Read-only mapping of each key to the active Lines instance.
-
-    Examples
-    --------
-    >>> model = AutoLines(3)
-    >>> from bluesky_widgets.jupyter.figures import JupyterFigures
-    >>> view = JupyterFigures(model.figures)
-    >>> model.add_run(run)
-    >>> model.add_run(another_run, pinned=True)
-    """
-
-    def __init__(self, max_runs):
-        self._max_runs = max_runs
-        super().__init__()
-
-    @property
-    def _plot_builder(self):
-        return Lines
-
-    @property
-    def _heuristic(self):
-        return infer_lines_to_plot
-
-    def _extra_kwargs(self):
-        return {"max_runs": self.max_runs}
-
-    @property
-    def max_runs(self):
-        return self._max_runs
-
-    @max_runs.setter
-    def max_runs(self, value):
-        self._max_runs = value
-        for instance in self._key_to_instance.values():
-            instance.max_runs = value
-
-    def add_run(self, run, pinned=False):
-        """
-        Add a Run.
-
-        Parameters
-        ----------
-        run : BlueskyRun
-        pinned : Boolean
-            If True, retain this Run until it is removed by the user.
-        """
-        # This just passes through to the base class, but we implement this
-        # method for the sake of providng a specific kwarg and docstring.
-        super().add_run(run, pinned=pinned)
-
-
-class AutoImages(Auto):
-    @property
-    def _plot_builder(self):
-        return Images
-
-    @property
-    def _heuristic(self):
-        return infer_images
+AutoLines = partial(AutoPlotter, plot_builder=Lines, heuristic=infer_lines_to_plot)
+AutoImages = partial(AutoPlotter, plot_builder=Images, heuristic=infer_images)
