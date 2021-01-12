8754de73f8a19f3fd830161ffba57dc3e8126e5a
Dan Allan <dallan@bnl.gov>
2021-01-12 09:13:17 -0500

    Tolerate empty object_keys when parsing hints.

--- bluesky_widgets/models/_heuristics.py
+++ bluesky_widgets/models/_heuristics.py
@@ -7,18 +7,22 @@ functionality.
 
 
 def hinted_fields(descriptor):
-    # Figure out which columns to put in the table.
-    obj_names = list(descriptor["object_keys"])
-    # We will see if these objects hint at whether
-    # a subset of their data keys ('fields') are interesting. If they
-    # did, we'll use those. If these didn't, we know that the RunEngine
-    # *always* records their complete list of fields, so we can use
-    # them all unselectively.
+    "Which columns are the most important ones to show and visualize?"
     columns = []
-    for obj_name in obj_names:
-        try:
-            fields = descriptor.get("hints", {}).get(obj_name, {})["fields"]
-        except KeyError:
-            fields = descriptor["object_keys"][obj_name]
-        columns.extend(fields)
+    if descriptor["object_keys"]:
+        # We will see if these objects hint at whether
+        # a subset of their data keys ('fields') are interesting. If they
+        # did, we'll use those. If these didn't, we know that the RunEngine
+        # *always* records their complete list of fields, so we can use
+        # them all unselectively.
+        for obj_name, all_fields in descriptor["object_keys"].items():
+            try:
+                fields = descriptor.get("hints", {}).get(obj_name, {})["fields"]
+            except KeyError:
+                fields = all_fields
+            columns.extend(fields)
+    else:
+        # There are no object_keys. This came from something other than the
+        # RunEngine. Just use all the columns.
+        columns.extend(descriptor["data_keys"])
     return columns
