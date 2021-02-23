859557cef25f62a55005f0fcfb3340e140e64dcd
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-02-23 12:03:47 -0500

    ENH: handling of feedback data received from RE Manager

--- bluesky_widgets/examples/advanced/qt_with_RE_worker.py
+++ bluesky_widgets/examples/advanced/qt_with_RE_worker.py
@@ -1,10 +1,7 @@
 """
 In one terminal start a RunEngine worker, which we will send commands to.
 
-PROFILE_DIR=$(python -c "import bluesky_queueserver; import pathlib; print(pathlib.Path(bluesky_queueserver.__file__).parent)")
-Start-re-manager \
-    --startup-dir $PROFILE_DIR/profile_collection_sim/ \
-    --zmq-data-proxy-addr localhost:5577
+start-re-manager --zmq-data-proxy-addr localhost:5577
 
 In another, start a message bus that will forward us Bluesky documents.
 
@@ -22,6 +19,7 @@ from bluesky_widgets.qt.figures import QtFigure
 from bluesky_widgets.qt import gui_qt
 from bluesky_queueserver.manager.comms import ZMQCommSendThreads
 import sys
+import time
 
 
 def main():
@@ -29,7 +27,46 @@ def main():
         worker_address, message_bus_address = sys.argv[1:]
         dispatcher = RemoteDispatcher(message_bus_address)
         client = ZMQCommSendThreads(zmq_server_address=worker_address)
-        client.send_message(method="environment_open")
+
+        # NOTE: this example starts only if RE Manager is idle and the queue is cleared.
+        #   Those are optional steps that ensure that the code in this example is executed correctly.
+
+        # Check if RE Worker environment already exists and RE manager is idle.
+        status = client.send_message(method="status")
+        if status["manager_state"] != "idle":
+            raise RuntimeError(
+                f"RE Manager state must be 'idle': current state: {status['manager_state']}"
+            )
+
+        # Clear the queue.
+        response = client.send_message(method="queue_clear")
+        if not response["success"]:
+            raise RuntimeError(f"Failed to clear the plan queue: {response['msg']}")
+
+        # Open the new environment only if it does not exist.
+        if not status["worker_environment_exists"]:
+            # Initiate opening of RE Worker environment
+            response = client.send_message(method="environment_open")
+            if not response["success"]:
+                raise RuntimeError(
+                    f"Failed to open RE Worker environment: {response['msg']}"
+                )
+
+            # Wait for the environment to be created.
+            t_timeout = 10
+            t_stop = time.time() + t_timeout
+            while True:
+                status2 = client.send_message(method="status")
+                if (
+                    status2["worker_environment_exists"]
+                    and status2["manager_state"] == "idle"
+                ):
+                    break
+                if time.time() > t_stop:
+                    raise RuntimeError("Failed to start RE Worker: timeout occurred")
+                time.sleep(0.5)
+
+        # Add plan to queue
         client.send_message(
             method="queue_item_add",
             params={
@@ -38,6 +75,7 @@ def main():
                 "user_group": "admin",
             },
         )
+
         model = Lines("motor", ["det"], max_runs=3)
         dispatcher.subscribe(stream_documents_into_runs(model.add_run))
         view = QtFigure(model.figure)
