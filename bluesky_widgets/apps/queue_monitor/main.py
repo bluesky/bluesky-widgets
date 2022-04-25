6beb13c31c0db192d4919e7baa08d771896701a2
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-04-25 11:47:58 -0400

    ENH: renamed CLI parameters and add deprecation messages

--- bluesky_widgets/apps/queue_monitor/main.py
+++ bluesky_widgets/apps/queue_monitor/main.py
@@ -11,6 +11,7 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description="BlueSky Queue Monitor")
     parser.add_argument(
         "--zmq-control-addr",
+        default=None,
         help="Address of control socket of RE Manager. If the address "
         "is passed as a CLI parameter, it overrides the address specified with "
         "QSERVER_ZMQ_CONTROL_ADDRESS environment variable. Default address is "
@@ -18,10 +19,12 @@ def main(argv=None):
     )
     parser.add_argument(
         "--zmq-control",
+        default=None,
         help="The parameter is deprecated and will be removed. Use --zmq-control-addr instead.",
     )
     parser.add_argument(
         "--zmq-info-addr",
+        default=None,
         help="Address of PUB-SUB socket of RE Manager. If the address "
         "is passed as a CLI parameter, it overrides the address specified with "
         "QSERVER_ZMQ_INFO_ADDRESS environment variable. Default address is "
@@ -29,6 +32,7 @@ def main(argv=None):
     )
     parser.add_argument(
         "--zmq-publish",
+        default=None,
         help="The parameter is deprecated and will be removed. Use --zmq-info-addr instead.",
     )
     args = parser.parse_args(argv)
@@ -36,12 +40,21 @@ def main(argv=None):
     # The priority is first to check if an address is passed as a parameter and then
     #   check if the environment variables are set. Otherwise use the default local addresses.
     #   (The default addresses are typically used in demos.)
-    zmq_control_addr = args.zmq_control or os.environ.get("QSERVER_ZMQ_CONTROL_ADDRESS", None)
-    zmq_info_addr = args.zmq_publish or os.environ.get("QSERVER_ZMQ_PUBLISH_ADDRESS", None)
+    zmq_control_addr = args.zmq_control_addr
+    zmq_control_addr = zmq_control_addr or args.zmq_control
+    if args.zmq_control is not None:
+        print("The parameter --zmq-control is deprecated and will be removed. Use --zmq-control-addr instead.")
+    zmq_control_addr = zmq_control_addr or os.environ.get("QSERVER_ZMQ_CONTROL_ADDRESS", None)
+
+    zmq_info_addr = args.zmq_info_addr
+    if args.zmq_publish is not None:
+        print("The parameter --zmq-publish is deprecated and will be removed. Use --zmq-info-addr instead.")
+    zmq_info_addr = zmq_info_addr or args.zmq_publish
+    zmq_info_addr = zmq_info_addr or os.environ.get("QSERVER_ZMQ_INFO_ADDRESS", None)
     if "QSERVER_ZMQ_PUBLISH_ADDRESS" in os.environ:
         print("WARNING: Environment variable QSERVER_ZMQ_PUBLISH_ADDRESS is deprecated and will be removed.")
-        print("    Use QSERVER_ZMQ_CONTROL_ADDRESS environment variable instead.")
-    zmq_info_addr = args.zmq_publish or os.environ.get("QSERVER_ZMQ_PUBLISH_ADDRESS", None)
+        print("    Use QSERVER_ZMQ_INFO_ADDRESS environment variable instead.")
+    zmq_info_addr = zmq_info_addr or os.environ.get("QSERVER_ZMQ_PUBLISH_ADDRESS", None)
     SETTINGS.zmq_re_manager_control_addr = zmq_control_addr
     SETTINGS.zmq_re_manager_info_addr = zmq_info_addr
 
