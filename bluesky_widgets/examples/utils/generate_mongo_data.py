cf8e1a6180914a46cd8c7e7b2158e35e9f580761
Dan Allan <dallan@bnl.gov>
2022-06-07 16:34:23 -0400

    App starts.

--- bluesky_widgets/examples/utils/generate_mongo_data.py
+++ bluesky_widgets/examples/utils/generate_mongo_data.py
@@ -1,12 +1,10 @@
-# Based on databroker/examples/generate_mongo_data.py
-from suitcase.mongo_normalized import Serializer
 from bluesky import RunEngine
 from bluesky.plans import count, scan
+from databroker.mongo_normalized import MongoAdapter
 from ophyd.sim import det, motor, SynSignal
+from tiled.client import from_tree
 import numpy as np
 import uuid
-
-from databroker._drivers.mongo_normalized import BlueskyMongoCatalog
 
 
 random_img = SynSignal(func=lambda: np.random.random((5, 10, 10)), name="random_img")
@@ -16,12 +14,11 @@ def get_catalog():
 
     RE = RunEngine()
 
-    mds = f"mongodb://localhost:27017/databroker-test-{uuid.uuid4()}"
-    fs = f"mongodb://localhost:27017/databroker-test-{uuid.uuid4()}"
-    serializer = Serializer(mds, fs)
+    uri = f"mongodb://localhost:27017/databroker-test-{uuid.uuid4()}"
+    adapter = MongoAdapter.from_uri(uri)
+    client = from_tree(adapter)
     for i in range(1, 5):
-        RE(scan([det], motor, -1, 1, 5 * i), serializer)
-    RE(count([random_img], 3), serializer)
+        RE(scan([det], motor, -1, 1, 5 * i), client.v1.insert)
+    RE(count([random_img], 3), client.v1.insert)
 
-    catalog = BlueskyMongoCatalog(mds, fs)
-    return catalog
+    return client
--- bluesky_widgets/models/search.py
+++ bluesky_widgets/models/search.py
@@ -1,9 +1,9 @@
-import abc
-import collections.abc
 from datetime import datetime, timedelta
 import itertools
 
 import dateutil.tz
+from databroker.queries import TimeRange, PartialUID, ScanID
+from tiled.queries import FullText
 
 from ..utils.list import EventedList
 from ..utils.event import EmitterGroup, Event
@@ -13,153 +13,8 @@ LOCAL_TIMEZONE = dateutil.tz.tzlocal()
 _epoch = datetime(1970, 1, 1, 0, 0, tzinfo=LOCAL_TIMEZONE)
 
 
-"""
-Vendored from databroker.queries
-"""
-
-
 class InvertedRange(ValueError):
     ...
-
-
-"""
-Vendored from databroker.queries
-"""
-
-
-class Query(collections.abc.Mapping):
-    """
-    This represents a MongoDB query.
-
-    MongoDB queries are typically encoded as simple dicts. This object supports
-    the dict interface in a read-only fashion. Subclassses add a nice __repr__
-    and mutable attributes from which the contents of the dict are derived.
-    """
-
-    @abc.abstractproperty
-    def query(self):
-        ...
-
-    @abc.abstractproperty
-    def kwargs(self):
-        ...
-
-    def __iter__(self):
-        return iter(self.query)
-
-    def __getitem__(self, key):
-        return self.query[key]
-
-    def __len__(self):
-        return len(self.query)
-
-    def replace(self, **kwargs):
-        """
-        Make a copy with parameters changed.
-        """
-        return type(self)(**{**self.kwargs, **kwargs})
-
-    def __repr__(self):
-        return f"{type(self).__name__}(" f"{', '.join(f'{k}={v}' for k, v in self.kwargs.items())})"
-
-
-class TextQuery(Query):
-    def __init__(self, text_search):
-        self._text_search = text_search
-
-    @property
-    def kwargs(self):
-        return {
-            "text_search": self._text_search,
-        }
-
-    @property
-    def query(self):
-        return {
-            "$text": {"$search": self._text_search},
-        }
-
-
-"""
-Vendored from databroker.queries
-"""
-
-
-class TimeRange(Query):
-    """
-    A search query representing a time range.
-
-    Parameters
-    ----------
-    since, until: dates gives as timestamp, datetime, or human-friendly string, optional
-    timezone : string
-        As in, 'US/Eastern'. If None is given, tzlocal is used.
-
-    Examples
-    --------
-    Any granularity (year, month, date, hour, minute, second) is accepted.
-
-    >>> TimeRange(since='2014')
-
-    >>> TimeRange(until='2019-07')
-
-    >>> TimeRange(since='2014-07-04', until='2020-07-04')
-
-    >>> TimeRange(since='2014-07-04 05:00')
-
-    Create a copy replacing some parameter. This leaves the original unchanged.
-
-    >>> tr = TimeRange(since='2014-07-04 05:00')
-    >>> tr.replace(until='2015')
-    TimeRange(since='2014-07-04 05:00', until='2015')
-    >>> tr
-    TimeRange(since='2014-07-04 05:00')
-
-    Access the raw query that this generates.
-
-    >>> TimeRange(since='2014').query
-    {'time': {'$gte': 1388552400.0}}
-    """
-
-    def __init__(self, since=None, until=None, timezone=None):
-        if timezone is None:
-            import tzlocal
-
-            timezone = tzlocal.get_localzone().zone
-        self.timezone = timezone
-        if since is None:
-            self._since_normalized = None
-        else:
-            self._since_normalized = normalize_human_friendly_time(since, tz=self.timezone)
-        self._since_raw = since
-        if until is None:
-            self._until_normalized = None
-        else:
-            self._until_normalized = normalize_human_friendly_time(until, tz=self.timezone)
-        self._until_raw = until
-        if since is not None and until is not None:
-            if self._since_normalized > self._until_normalized:
-                raise InvertedRange("since must not be greater than until.")
-
-    @property
-    def kwargs(self):
-        return {
-            "since": self._since_raw,
-            "until": self._until_raw,
-            "timezone": self.timezone,
-        }
-
-    @property
-    def query(self):
-        query = {"time": {}}
-        if self._since_normalized is not None:
-            query["time"]["$gte"] = self._since_normalized
-        if self._until_normalized is not None:
-            query["time"]["$lt"] = self._until_normalized
-        if query["time"]:
-            return query
-        else:
-            return {}
 
 
 # The following are vendored from databroker.utils.
@@ -268,7 +123,7 @@ class SearchInput:
         self._fields = fields or []
         self._field_search = UpdateOnlyDict(dict.fromkeys(self._fields))
         self._text = None
-        self._query = {}
+        self._query = {"fields": {}}
         self.events = EmitterGroup(
             source=self,
             auto_connect=True,
@@ -381,16 +236,16 @@ class SearchInput:
     def on_field_search_updated(self, event):
         for field, text in event.update.items():
             if not text:
-                self._query.pop(field, None)
+                self._query["fields"].pop(field, None)
             else:
-                self._query.update({field: text})
+                self._query["fields"][fields] = text
         self.events.query(query=self._query)
 
     def on_text(self, event):
         if not event.text:
-            self._query.pop("$text", None)
+            self._query.pop("text", None)
         else:
-            self._query.update(TextQuery(event.text))
+            self._query["text"] = FullText(event.text)
         self.events.query(query=self._query)
 
     def on_since(self, event):
@@ -402,7 +257,7 @@ class SearchInput:
             self.until = event.date
             return
         if tr:
-            self._query.update(tr)
+            self._query["time"] = tr
         else:
             self._query.pop("time", None)
         self.events.query(query=self._query)
@@ -416,7 +271,7 @@ class SearchInput:
             self.since = event.date
             return
         if tr:
-            self._query.update(tr)
+            self._query["time"] = tr
         else:
             self._query.pop("time", None)
         self.events.query(query=self._query)
@@ -547,7 +402,7 @@ class SearchResults:
     @property
     def selection_as_catalog(self):
         "A Catalog containing the selected rows"
-        return self.catalog.search({"uid": {"$in": self.selected_uids}})
+        return self.catalog.search(PartialUID(*self.selected_uids))
 
 
 class RunSearch:
@@ -562,7 +417,7 @@ class RunSearch:
         # the mongoquery library used by JSONL and msgpack databroker drivers
         # or any other "mock" in-memory MongoDB imitators that we know of.
         try:
-            catalog.search({"$text": ""})
+            catalog.search(FullText(""))
         except NotImplementedError:
             text_search_supported = False
         else:
@@ -580,7 +435,15 @@ class RunSearch:
         self.search_results.events.end_reset()
 
     def _on_query(self, event):
-        results = self.catalog.search(event.query)
+        results = self.catalog
+        text = event.query.get("text")
+        if text:
+            results = results.search(text)
+        time = event.query.get("time")
+        if time:
+            results = results.search(time)
+        for q in event.query.get("fields", []):
+            results = results.search(q)
         self.search_results.catalog = results
 
 
