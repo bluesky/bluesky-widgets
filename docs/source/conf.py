7c55f1f7dc82a9df9ce7d5a6230e2dfce75148c1
Charles Titus <ctitus@bnl.gov>
2026-03-19 14:01:36 -0400

    Fixing additional lint problem due to now-unused import in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,7 +107,6 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-import sphinx_rtd_theme
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
